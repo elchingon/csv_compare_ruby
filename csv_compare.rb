d23f115caff5430cfe17965a13e8c3fe346ae0da
Alexi Carey <ciegoproductions@gmail.com>
2015-05-18 14:50:23 -0600

    Add output with counts, add second compare csv

--- csv_compare.rb
+++ csv_compare.rb
@@ -7,12 +7,20 @@ require 'csv'
 targeted_file = ARGV[0]
 compare_file = ARGV[1]
 
+count_found, total_count = 0, 0
+ 
 CSV.open("new_" + targeted_file.to_s, "wb") do |csv|
   CSV.foreach(targeted_file) do |row|
+    total_count += 1 
     record_found = "No"
+    puts "Comparing row ##{total_count}"
+
     CSV.foreach(compare_file) do |compare_row|
       if compare_row[1] == row[1]
 	record_found = "Yes"
+	count_found += 1
+	
+	puts "Row Found - #{compare_row[1]}" 
 	break
       end
     end
@@ -20,3 +28,6 @@ CSV.open("new_" + targeted_file.to_s, "wb") do |csv|
     csv << ["#{row[0]}","#{row[1]}", record_found]
   end
 end
+
+puts "Total Rows Compared #{total_count}"
+puts "Total Rows Found #{count_found}"
